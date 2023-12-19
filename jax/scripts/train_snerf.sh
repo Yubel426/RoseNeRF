4c62636814c27137fdcdbdc4ba51c7543ac783ae
liang <liang.com>
2023-12-19 15:18:57 +0800

    add sh

---
+++ jax/scripts/train_snerf.sh
@@ -0,0 +1,14 @@
+export CUDA_VISIBLE_DEVICES=7
+
+SCENE=dolphin
+EXPERIMENT=logs_snerf
+DATA_DIR=/home/nxt/nxtdg/datasets
+CHECKPOINT_DIR=/home/nxt/nxtdg/logs/"$EXPERIMENT"/"$SCENE"
+
+python -m train \
+  --gin_configs=configs/snerf_360.gin \
+  --gin_bindings="Config.dataset_loader = 'llff'" \
+  --gin_bindings="Config.factor = 0" \
+  --gin_bindings="Config.data_dir = '${DATA_DIR}/${SCENE}'" \
+  --gin_bindings="Config.checkpoint_dir = '${CHECKPOINT_DIR}'" \
+  --logtostderr
